b4c0d64e1e6e1e5b8a355fd75eaea52ba79e5341
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2021-03-14 15:37:39 +0100

    few changes

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Todo } from "./todo.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataService {
+  todos: Todo[] = [
+    new Todo("This is one of my best life so far"),
+    new Todo(
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum atque necessitatibus ipsam voluptate tempora deleniti sit aperiam dolores dicta.Ex illum quaerat nobis tenetur dolore sed.Dolorem non molestiae inventore!"
+    ),
+  ];
+
+  constructor() {}
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodos(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "../shared/todo.model";
+import { DataService } from "../shared/data.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.scss"],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos();
   }
 
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) return alert("Form is invalid");
+    console.log("form submited");
+    console.log(form);
+    this.dataService.addTodos(new Todo(form.value.text));
+  }
 }
